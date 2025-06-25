0d62fa1c719eb2286fe3e7ccb9a45b474c7423e9
Leidson Gomes <leidsonglag@outlook.com>
2025-06-25 17:58:18 -0300

    Remove test component and tailwind config file

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,8 @@
 import './bootstrap';
 
 import { createApp } from 'vue/dist/vue.esm-bundler.js';
-import TestComponent from './components/Test.vue';
 import LoginFormComponent from './components/LoginForm.vue';
 
 const app = createApp({});
-app.component('test-component', TestComponent);
 app.component('login-form-component', LoginFormComponent);
 app.mount('#app');
